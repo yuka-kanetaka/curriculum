62332ec720d0aaa77c7c8ace2bd1fb7a98b0f6cc
yuka <73115085+yuzponzoo@users.noreply.github.com>
2020-11-04 21:29:53 +0900

    課題提出テスト
    Task1_1の作成

---
+++ JavaStudy/1-1/Task1_1.java
@@ -0,0 +1,33 @@
+public class Task1_1 {
+
+    public static void main(String[] args) {
+
+        // ①int型の hogeInt という名前の変数を宣言し、値「50」で初期化
+        int hogeInt = 50;
+
+        // ②Stringクラスの hogeString という名前の変数を宣言、値「テスト」で初期化
+        String hogestring = "テスト";
+
+        // ③下記の処理について、何をしているのかコメントを記入してください
+        // [double型のhogeDoubleという名前の変数を宣言して、値「0.2」で初期化]
+        double hogeDouble = 0.2;
+
+        // ④変数 hogeInt を値「100」で上書き
+         hogeInt = 100;
+
+        // ⑤変数 hogeString を値「合格」で上書き
+         hogestring = "合格";
+
+        // ⑥変数 hogeInt の値を表示
+        System.out.println(hogeInt);
+
+        // ⑦下記の処理について、何をしているのかコメントを記入してください
+        // 変数hogestringの値を表示
+        System.out.println(hogestring);
+
+        // ⑧変数 hogeDouble の値を表示
+        System.out.println(hogeDouble);
+
+
+    }
+}
